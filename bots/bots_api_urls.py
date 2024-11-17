cee91ac7669d3d48e4abb73baa9f499e710fb831
Noah Duncan <crusty.noah@gmail.com>
2024-11-17 14:52:21 -0600

    fix urls

--- bots/bots_api_urls.py
+++ bots/bots_api_urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import bots_api_views
 
 urlpatterns = [
-    path('', bots_api_views.BotCreateView.as_view(), name='bot-create'),
-    path('/<str:object_id>', bots_api_views.BotDetailView.as_view(), name='bot-detail'),
-    path('/<str:object_id>/leave', bots_api_views.BotLeaveView.as_view(), name='bot-leave'),
-    path('/<str:object_id>/transcript', bots_api_views.TranscriptView.as_view(), name='bot-transcript'),
+    path('bots', bots_api_views.BotCreateView.as_view(), name='bot-create'),
+    path('bots/<str:object_id>', bots_api_views.BotDetailView.as_view(), name='bot-detail'),
+    path('bots/<str:object_id>/leave', bots_api_views.BotLeaveView.as_view(), name='bot-leave'),
+    path('bots/<str:object_id>/transcript', bots_api_views.TranscriptView.as_view(), name='bot-transcript'),
 ]
 
 # catch any other paths and return a 404 json response - must be last
