d4da823a708b9fa4f13fe2908a03f42ebd9b6a95
Noah Duncan <crusty.noah@gmail.com>
2024-12-03 23:53:24 -0600

    did nasty stuff and got spacing working

--- bots/templatetags/bot_filters.py
+++ bots/templatetags/bot_filters.py
@@ -8,4 +8,11 @@ def modulo(num, val):
 
 @register.filter
 def integer_divide(num, val):
-    return int(num) // val 
+    return int(num) // val
+
+@register.filter
+def get_next(value, current_index):
+    try:
+        return value[current_index + 1]
+    except IndexError:
+        return value[current_index]  # fallback to current item if next doesn't exist 
