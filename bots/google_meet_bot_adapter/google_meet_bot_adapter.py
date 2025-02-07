f6229d5ea0195c2af1c0b05bea1a435dc30a59d3
Noah Duncan <crusty.noah@gmail.com>
2025-02-07 10:07:30 -0600

    try again

--- bots/google_meet_bot_adapter/google_meet_bot_adapter.py
+++ bots/google_meet_bot_adapter/google_meet_bot_adapter.py
@@ -301,6 +301,7 @@ class GoogleMeetBotAdapter(BotAdapter):
         options.add_argument("--disable-dev-shm-usage")
         log_path = "chromedriver.log"
 
+        subprocess.run(["rm", "-rf", os.path.expanduser("~/.local/share/undetected_chromedriver")])
         self.driver = uc.Chrome(service_log_path=log_path, use_subprocess=True, options=options, version_main=132)
 
         self.driver.set_window_size(1920, 1080)
