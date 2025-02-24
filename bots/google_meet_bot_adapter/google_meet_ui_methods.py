196aa3e8631efa7fb3bc6831ea6bcab598b0fff5
Noah Duncan <crusty.noah@gmail.com>
2025-02-24 11:54:40 -0600

    wait 5 minutes to be let in, if were still waiting give up and say request to join was denied

--- bots/google_meet_bot_adapter/google_meet_ui_methods.py
+++ bots/google_meet_bot_adapter/google_meet_ui_methods.py
@@ -79,6 +79,15 @@ class GoogleMeetUIMethods:
             print("Someone in the call denied our request to join. Raising UiRequestToJoinDeniedException")
             raise UiRequestToJoinDeniedException("Someone in the call denied your request to join", step)
 
+    def look_for_asking_to_be_let_in_element(self, step):
+        asking_to_be_let_in_element = self.find_element_by_selector(
+            By.XPATH,
+            '//*[contains(text(), "Asking to be let in")]',
+        )
+        if asking_to_be_let_in_element:
+            print("Bot was not let in after waiting period expired. Raising UiRequestToJoinDeniedException")
+            raise UiRequestToJoinDeniedException("Bot was not let in after waiting period expired", step)
+
     def fill_out_name_input(self):
         num_attempts_to_look_for_name_input = 30
         print("Waiting for the name input field...")
@@ -101,7 +110,7 @@ class GoogleMeetUIMethods:
                 raise UiCouldNotLocateElementException("Could not find name input. Unknown error.", "name_input", e)
 
     def click_captions_button(self):
-        num_attempts_to_look_for_captions_button = 120
+        num_attempts_to_look_for_captions_button = 300
         print("Waiting for captions button...")
         for attempt_to_look_for_captions_button_index in range(num_attempts_to_look_for_captions_button):
             try:
@@ -117,6 +126,8 @@ class GoogleMeetUIMethods:
 
                 last_check_timed_out = attempt_to_look_for_captions_button_index == num_attempts_to_look_for_captions_button - 1
                 if last_check_timed_out:
+                    self.look_for_asking_to_be_let_in_element("click_captions_button")
+
                     print("Could not find captions button. Timed out. Raising UiCouldNotLocateElementException")
                     raise UiCouldNotLocateElementException(
                         "Could not find captions button. Timed out.",
