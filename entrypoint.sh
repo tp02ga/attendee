7ff7c3de730ef41f722da13b4608af0d8840c6b1
Noah Duncan <crusty.noah@gmail.com>
2025-03-28 16:25:34 -0500

    sp

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+# cleanup to be "stateless" on startup, otherwise pulseaudio daemon can't start
+rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+
+# start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+
+# create a virtual audio sink (speaker)
+echo "Creating virtual audio sink (speaker): ";
+pactl load-module module-virtual-sink sink_name=VirtualSpeaker format=s16le
+
+# sllow pulse audio to be accssed via TCP (from localhost only), to allow other users to access the virtual devices
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
+
+# https://manpages.ubuntu.com/manpages/trusty/en/man5/pulse-client.conf.5.html
+mkdir -p /home/.pulse
+echo "default-server = 127.0.0.1" > /home/.pulse/client.conf
+
+# set VirtualSpeaker as default output sink;
+echo "Setting default sink: ";
+pactl set-default-sink VirtualSpeaker
