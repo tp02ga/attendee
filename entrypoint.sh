7e1a0671b1239f5bbf1073edaba189fd76bdbf8f
Noah Duncan <crusty.noah@gmail.com>
2025-03-28 17:00:24 -0500

    sp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/run/dbus
-dbus-uuidgen > /var/lib/dbus/machine-id
-dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
-
-# Clean up to be "stateless" on startup, otherwise pulseaudio daemon can't start
+# Clean up old PulseAudio state
 rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 
-# Start pulseaudio as system-wide daemon; for debugging it helps to start in non-daemon mode
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+# Start PulseAudio as user
+pulseaudio --start --exit-idle-time=-1 -v
 
-# Create a virtual audio sink (i.e., a "virtual speaker") 
-# Note: You can also specify other parameters such as format, channels, etc. if needed. 
-echo "Creating virtual audio sink: "
-pactl load-module module-virtual-sink sink_name=VirtualSpeaker master=auto_null format=s16le
-
-# Allow pulseaudio to be accessed via TCP (from localhost only), to allow other users to access the virtual devices
-pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
-
-# Configure client.conf to use our local Pulseaudio server
-mkdir -p /home/.pulse
-echo "default-server = 127.0.0.1" > /home/.pulse/client.conf
-
-# Set VirtualSpeaker as default sink (i.e. default output device)
-echo "Setting default sink: "
+# Load a virtual sink (speaker)
+pactl load-module module-null-sink sink_name=VirtualSpeaker
 pactl set-default-sink VirtualSpeaker
