597c8c008504f1e1a9ff31b437c0177a62b9c8d0
Noah Duncan <crusty.noah@gmail.com>
2025-03-28 16:57:43 -0500

    sp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p /var/run/dbus
+dbus-uuidgen > /var/lib/dbus/machine-id
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 # Clean up to be "stateless" on startup, otherwise pulseaudio daemon can't start
 rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 
