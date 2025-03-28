ff573665843c1aedef7e1976279316082fde03b8
Noah Duncan <crusty.noah@gmail.com>
2025-03-28 16:31:38 -0500

    sp

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 # start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
 pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
-# create a virtual audio sink (speaker)
-echo "Creating virtual audio sink (speaker): ";
-pactl load-module module-virtual-sink sink_name=VirtualSpeaker format=s16le
+# create a virtual audio source; fixed by adding source master and format
+echo "Creating virtual audio source: ";
+pactl load-module module-virtual-source master=auto_null.monitor format=s16le source_name=VirtualMic
 
 # sllow pulse audio to be accssed via TCP (from localhost only), to allow other users to access the virtual devices
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
@@ -15,6 +15,6 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
 mkdir -p /home/.pulse
 echo "default-server = 127.0.0.1" > /home/.pulse/client.conf
 
-# set VirtualSpeaker as default output sink;
-echo "Setting default sink: ";
-pactl set-default-sink VirtualSpeaker
+# set VirtualMic as default input source;
+echo "Setting default source: ";
+pactl set-default-source VirtualMic
