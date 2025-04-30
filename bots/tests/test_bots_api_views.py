d21ccc6fcfc5595115b7315754ab26b313d6497b
Rex Posadas <rexposadas@gmail.com>
2025-04-29 19:33:30 -0700

    added test

---
+++ bots/tests/test_bots_api_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from bots.models import Project, Credentials
+from bots.bots_api_views import validate_meeting_url_and_credentials
+from accounts.models import Organization
+
+
+class TestValidateMeetingUrlAndCredentials(TestCase):
+    def setUp(self):
+        # Create organization first since it's required for Project
+        organization = Organization.objects.create(name="Test Organization")
+        self.project = Project.objects.create(name="Test Project", organization=organization)
+
+    def test_validate_google_meet_url(self):
+        """Test Google Meet URL validation"""
+        # Valid Google Meet URL
+        error = validate_meeting_url_and_credentials("https://meet.google.com/abc-defg-hij", self.project)
+        self.assertIsNone(error)
+
+        # Invalid Google Meet URL format
+        error = validate_meeting_url_and_credentials("http://meet.google.com/abc-defg-hij", self.project)
+        self.assertEqual(error, "Google Meet URL must start with https://meet.google.com/")
+
+    def test_validate_zoom_url_and_credentials(self):
+        """Test Zoom URL and credentials validation"""
+        # Test Zoom URL without credentials
+        error = validate_meeting_url_and_credentials("https://zoom.us/j/123456789", self.project)
+        self.assertEqual(error, "Zoom App credentials are required to create a Zoom bot")
+
+    def test_validate_teams_url(self):
+        """Test Teams URL validation"""
+        # Teams URLs don't require specific validation
+        error = validate_meeting_url_and_credentials("https://teams.microsoft.com/meeting/123", self.project)
+        self.assertIsNone(error)
