c12810b59bb0050e1c32205be1371833697ec14f
Noah Duncan <crusty.noah@gmail.com>
2025-05-08 01:34:28 -0500

    improve

--- bots/utils.py
+++ bots/utils.py
@@ -392,7 +392,14 @@ def generate_utterance_json_for_bot_detail_view(recording):
 def root_domain_from_url(url):
     if not url:
         return None
-    return tldextract(url).registered_domain
+    return tldextract.extract(url).registered_domain
+
+
+def domain_and_subdomain_from_url(url):
+    if not url:
+        return None
+    extract_from_url = tldextract.extract(url)
+    return extract_from_url.subdomain + "." + extract_from_url.registered_domain
 
 
 def meeting_type_from_url(url):
@@ -400,12 +407,13 @@ def meeting_type_from_url(url):
         return None
 
     root_domain = root_domain_from_url(url)
+    domain_and_subdomain = domain_and_subdomain_from_url(url)
 
     if root_domain == "zoom.us":
         return MeetingTypes.ZOOM
-    elif root_domain == "meet.google.com":
+    elif domain_and_subdomain == "meet.google.com":
         return MeetingTypes.GOOGLE_MEET
-    elif root_domain == "teams.microsoft.com" or root_domain == "teams.live.com":
+    elif domain_and_subdomain == "teams.microsoft.com" or domain_and_subdomain == "teams.live.com":
         return MeetingTypes.TEAMS
     else:
         return None
