eb79f3595e0bfe0274db2c5aaee431d17e3d3e1a
Noah Duncan <crusty.noah@gmail.com>
2025-05-10 13:39:34 -0500

    handle ec where recording in progress not found

--- bots/bot_controller/bot_controller.py
+++ bots/bot_controller/bot_controller.py
@@ -639,7 +639,8 @@ class BotController:
     def get_recording_in_progress(self):
         recordings_in_progress = Recording.objects.filter(bot=self.bot_in_db, state=RecordingStates.IN_PROGRESS)
         if recordings_in_progress.count() == 0:
-            raise Exception("No recording in progress found")
+            logger.warning(f"Warning: No recording in progress found in get_recording_in_progress for bot {self.bot_in_db.object_id}")
+            return None
         if recordings_in_progress.count() > 1:
             raise Exception(f"Expected at most one recording in progress for bot {self.bot_in_db.object_id}, but found {recordings_in_progress.count()}")
         return recordings_in_progress.first()
@@ -656,6 +657,8 @@ class BotController:
 
         # Create new utterance record
         recording_in_progress = self.get_recording_in_progress()
+        if recording_in_progress is None:
+            return
         source_uuid = f"{recording_in_progress.object_id}-{message['source_uuid_suffix']}"
         utterance, _ = Utterance.objects.update_or_create(
             recording=recording_in_progress,
@@ -689,6 +692,8 @@ class BotController:
 
         # Create new utterance record
         recording_in_progress = self.get_recording_in_progress()
+        if recording_in_progress is None:
+            return
         utterance = Utterance.objects.create(
             source=Utterance.Sources.PER_PARTICIPANT_AUDIO,
             recording=recording_in_progress,
