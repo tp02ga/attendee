437a3bd8d0ed5a6909efcf52513b20712cf9972b
Noah Duncan <crusty.noah@gmail.com>
2025-06-30 10:59:46 -0500

    tweak docs

--- bots/serializers.py
+++ bots/serializers.py
@@ -406,7 +406,7 @@ class BotChatMessageRequestSerializer(serializers.Serializer):
         "properties": {
             "audio_url": {
                 "type": "string",
-                "description": "The URL of the websocket to use for receiving meeting audio in real time and having the bot output audio in real time. It must start with ws:// or wss://.",
+                "description": "The URL of the websocket to use for receiving meeting audio in real time and having the bot output audio in real time. It must start with ws:// or wss://. See https://docs.attendee.dev/guides/realtime-audio for details on how to receive and send audio through the websocket connection.",
             },
         },
         "required": [],
