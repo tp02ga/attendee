dd385defab27774094e5fd72cee092b7c7c4ca91
Noah Duncan <crusty.noah@gmail.com>
2025-07-08 23:03:35 -0500

    sp

--- bots/zoom_web_bot_adapter/zoom_web_chromedriver_page.js
+++ bots/zoom_web_bot_adapter/zoom_web_chromedriver_page.js
@@ -1,14 +1,16 @@
-const client = ZoomMtgEmbedded.createClient()
-
-let meetingSDKElement = document.getElementById('meetingSDKElement')
+ZoomMtg.preLoadWasm()
+ZoomMtg.prepareWebSDK()
 
 var authEndpoint = ''
 var sdkKey = ''
-var meetingNumber = 84315220467
-var passWord = ''
+var meetingNumber = 1
+var passWord = '.1'
 var role = 0
 var userName = 'Zoom Web Bot'
 var userEmail = ''
+var registrantToken = ''
+var zakToken = ''
+var leaveUrl = 'https://zoom.us'
 var registrantToken = ''
 var zakToken = ''
 
@@ -19,22 +21,81 @@ function joinMeeting() {
 
 function startMeeting(signature) {
 
-  client.init({zoomAppRoot: meetingSDKElement, language: 'en-US', patchJsMedia: true, leaveOnPageUnload: true}).then(() => {
-    client.join({
-      signature: signature,
-      sdkKey: sdkKey,
-      meetingNumber: meetingNumber,
-      password: passWord,
-      userName: userName,
-      userEmail: userEmail,
-      tk: registrantToken,
-      zak: zakToken
-    }).then(() => {
-      console.log('joined successfully')
-    }).catch((error) => {
-      console.log(error)
+  document.getElementById('zmmtg-root').style.display = 'block'
+
+    ZoomMtg.init({
+    leaveUrl: leaveUrl,
+    patchJsMedia: true,
+    leaveOnPageUnload: true,
+    disableZoomLogo: true,
+    disablePreview: true,
+    disableJoinAudio: true,
+    isSupportAV: false,
+    success: (success) => {
+        console.log(success)
+        ZoomMtg.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        passWord: passWord,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+        zak: zakToken,
+        success: (success) => {
+            console.log(success)
+        },
+        error: (error) => {
+            console.log(error)
+        },
+        })
+    },
+    error: (error) => {
+        console.log(error)
+    }
     })
-  }).catch((error) => {
-    console.log(error)
-  })
+
+    ZoomMtg.inMeetingServiceListener('onJoinSpeed', function (data) {
+        console.log('onJoinSpeed', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onMeetingStatus', function (data) {
+        console.log('onMeetingStatus', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onReceiveTranscriptionMsg', function (data) {
+    console.log('onReceiveTranscriptionMsg', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onReceiveChatMsg', function (data) {
+    console.log('onReceiveChatMsg', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserJoin', function (data) {
+    console.log('onUserJoin', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserLeave', function (data) {
+    console.log('onUserLeave', data);
+    // reasonCode Return the reason the current user left.
+    const reasonCode = {
+        OTHER: 0, // Other reason.
+        HOST_ENDED_MEETING: 1, // Host ended the meeting.
+        SELF_LEAVE_FROM_IN_MEETING: 2, // User (self) left from being in the meeting.
+        SELF_LEAVE_FROM_WAITING_ROOM: 3, // User (self) left from the waiting room.
+        SELF_LEAVE_FROM_WAITING_FOR_HOST_START: 4, // User (self) left from waiting for host to start the meeting.
+        MEETING_TRANSFER: 5, // The meeting was transferred to another end to open.
+        KICK_OUT_FROM_MEETING: 6, // Removed from meeting by host or co-host.
+        KICK_OUT_FROM_WAITING_ROOM: 7, // Removed from waiting room by host or co-host.
+        LEAVE_FROM_DISCLAIMER: 8, // User click cancel in disclaimer dialog 
+    };
+
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserUpdate', function (data) {
+    console.log('onUserUpdate', data);
+    });
 }
+
+// When dom loads call the joinMeeting function
+document.addEventListener('DOMContentLoaded', joinMeeting);
