dd4d367d4eaf00b197a8b426471a242743e11503
Noah Duncan <crusty.noah@gmail.com>
2025-07-19 11:31:15 -0500

    add waiting room and waiting for host timeout

--- bots/zoom_web_bot_adapter/zoom_web_ui_methods.py
+++ bots/zoom_web_bot_adapter/zoom_web_ui_methods.py
@@ -1,13 +1,16 @@
+import logging
 import os
 import time
-import logging
+
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from bots.web_bot_adapter.ui_methods import UiCouldNotClickElementException, UiCouldNotJoinMeetingWaitingForHostException, UiCouldNotJoinMeetingWaitingRoomTimeoutException, UiCouldNotLocateElementException, UiLoginRequiredException, UiMeetingNotFoundException, UiRequestToJoinDeniedException, UiRetryableExpectedException
+
+from bots.web_bot_adapter.ui_methods import UiCouldNotJoinMeetingWaitingForHostException, UiCouldNotJoinMeetingWaitingRoomTimeoutException, UiCouldNotLocateElementException
 
 logger = logging.getLogger(__name__)
+
 
 class ZoomWebUIMethods:
     def __init__(self, driver):
@@ -16,9 +19,9 @@ class ZoomWebUIMethods:
     def attempt_to_join_meeting(self):
         # Get the directory of the current file and construct path to HTML file
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        html_file_path = os.path.join(current_dir, 'zoom_web_chromedriver_page.html')
-        file_url = f'file://{html_file_path}'
-        
+        html_file_path = os.path.join(current_dir, "zoom_web_chromedriver_page.html")
+        file_url = f"file://{html_file_path}"
+
         self.driver.get(file_url)
 
         self.driver.execute_cdp_cmd(
@@ -42,35 +45,26 @@ class ZoomWebUIMethods:
 
         # Then find a button with the arial-label "More meeting control " and click it
         logger.info("Waiting for more meeting control button")
-        more_meeting_control_button = WebDriverWait(self.driver, 60).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[aria-label='More meeting control']"))
-        )
+        more_meeting_control_button = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[aria-label='More meeting control']")))
         logger.info("More meeting control button found, clicking")
         self.driver.execute_script("arguments[0].click();", more_meeting_control_button)
 
-
         # Then find an <a> tag with the arial label "Captions" and click it
         logger.info("Waiting for captions button")
-        captions_button = WebDriverWait(self.driver, 60).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "a[aria-label='Captions']"))
-        )
+        captions_button = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[aria-label='Captions']")))
         logger.info("Captions button found, clicking")
         self.driver.execute_script("arguments[0].click();", captions_button)
 
         # Then find an <a> tag with the arial label "Your caption settings grouping Show Captions" and click it
         logger.info("Waiting for your caption settings grouping Show Captions button")
-        your_caption_settings_grouping_show_captions_button = WebDriverWait(self.driver, 60).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "a[aria-label='Your caption settings grouping Show Captions']"))
-        )
+        your_caption_settings_grouping_show_captions_button = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[aria-label='Your caption settings grouping Show Captions']")))
         logger.info("Your caption settings grouping Show Captions button found, clicking")
         self.driver.execute_script("arguments[0].click();", your_caption_settings_grouping_show_captions_button)
-        
+
         # Then see if it created a modal to select the caption language. If so, just click the save button
         try:
             logger.info("Waiting for save button")
-            save_button = WebDriverWait(self.driver, 2).until(
-                EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'zm-btn--primary') and contains(text(), 'Save')]"))
-            )
+            save_button = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'zm-btn--primary') and contains(text(), 'Save')]")))
             logger.info("Save button found, clicking")
             self.driver.execute_script("arguments[0].click();", save_button)
         except TimeoutException:
@@ -80,9 +74,7 @@ class ZoomWebUIMethods:
         # Then see if it created a modal to confirm that the meeting is being transcribed.
         try:
             logger.info("Waiting for OK button")
-            ok_button = WebDriverWait(self.driver, 2).until(
-                EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'zm-btn--primary') and contains(text(), 'OK')]"))
-            )
+            ok_button = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'zm-btn--primary') and contains(text(), 'OK')]")))
             logger.info("OK button found, clicking")
             self.driver.execute_script("arguments[0].click();", ok_button)
         except TimeoutException:
@@ -95,18 +87,20 @@ class ZoomWebUIMethods:
         self.driver.execute_script("leaveMeeting()")
 
     def click_cancel_join_button(self):
-        cancel_join_button = WebDriverWait(self.driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button.leave-btn"))
-        )
+        cancel_join_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.leave-btn")))
         logger.info("Cancel join button found, clicking")
         self.driver.execute_script("arguments[0].click();", cancel_join_button)
 
-    def check_if_waiting_room_timeout_exceeded(self, waiting_room_timeout_started_at, step):
-        waiting_room_timeout_exceeded = time.time() - waiting_room_timeout_started_at > self.automatic_leave_configuration.waiting_room_timeout_seconds
-        if waiting_room_timeout_exceeded:
+    def check_if_timeout_exceeded(self, timeout_started_at, step, is_waiting_for_host_to_start_meeting):
+        if is_waiting_for_host_to_start_meeting:
+            timeout_exceeded = time.time() - timeout_started_at > self.automatic_leave_configuration.wait_for_host_to_start_meeting_timeout_seconds
+        else:
+            timeout_exceeded = time.time() - timeout_started_at > self.automatic_leave_configuration.waiting_room_timeout_seconds
+
+        if timeout_exceeded:
             # If there is more than one participant in the meeting, then the bot was just let in and we should not timeout
             if len(self.participants_info) > 1:
-                logger.info("Waiting room timeout exceeded, but there is more than one participant in the meeting. Not aborting join attempt.")
+                logger.info(f"Timeout exceeded, but there is more than one participant in the meeting. Not aborting join attempt. is_waiting_for_host_to_start_meeting={is_waiting_for_host_to_start_meeting}")
                 return
 
             try:
@@ -115,24 +109,45 @@ class ZoomWebUIMethods:
                 logger.info("Error clicking cancel join button, but not a fatal error")
 
             self.abort_join_attempt()
-            logger.info("Waiting room timeout exceeded. Raising UiCouldNotJoinMeetingWaitingRoomTimeoutException")
-            raise UiCouldNotJoinMeetingWaitingRoomTimeoutException("Waiting room timeout exceeded", step)
+
+            if is_waiting_for_host_to_start_meeting:
+                logger.info("Waiting for host to start meeting timeout exceeded. Raising UiCouldNotJoinMeetingWaitingForHostToStartMeetingException")
+                raise UiCouldNotJoinMeetingWaitingForHostException("Waiting for host to start meeting timeout exceeded", step)
+            else:
+                logger.info("Waiting room timeout exceeded. Raising UiCouldNotJoinMeetingWaitingRoomTimeoutException")
+                raise UiCouldNotJoinMeetingWaitingRoomTimeoutException("Waiting room timeout exceeded", step)
 
     def click_join_audio_button(self):
-        num_attempts_to_look_for_join_audio_button = self.automatic_leave_configuration.waiting_room_timeout_seconds * 10
+        num_attempts_to_look_for_join_audio_button = (self.automatic_leave_configuration.waiting_room_timeout_seconds + self.automatic_leave_configuration.wait_for_host_to_start_meeting_timeout_seconds) * 10
         logger.info("Waiting for join audio button...")
-        waiting_room_timeout_started_at = time.time()
-        
+        timeout_started_at = time.time()
+
+        # We can either be waiting for the host to start meeting or we can be waiting to be admitted to the meeting
+        is_waiting_for_host_to_start_meeting = False
+
         for attempt_index in range(num_attempts_to_look_for_join_audio_button):
             try:
-                join_audio_button = WebDriverWait(self.driver, 1).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, "button.join-audio-by-voip__join-btn"))
-                )
+                join_audio_button = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.join-audio-by-voip__join-btn")))
                 logger.info("Join audio button found")
                 self.driver.execute_script("arguments[0].click();", join_audio_button)
                 return
             except TimeoutException as e:
-                self.check_if_waiting_room_timeout_exceeded(waiting_room_timeout_started_at, "click_join_audio_button")
+                previous_is_waiting_for_host_to_start_meeting = is_waiting_for_host_to_start_meeting
+                try:
+                    is_waiting_for_host_to_start_meeting = self.driver.find_element(
+                        By.XPATH,
+                        '//*[contains(text(), "for host to start the meeting")]',
+                    ).is_displayed()
+                except:
+                    is_waiting_for_host_to_start_meeting = False
+
+                logger.info(f"is_waiting_for_host_to_start_meeting={is_waiting_for_host_to_start_meeting}")
+
+                # If we switch from waiting for the host to start the meeting to waiting to be admitted to the meeting, then we need to reset the timeout
+                if previous_is_waiting_for_host_to_start_meeting != is_waiting_for_host_to_start_meeting:
+                    timeout_started_at = time.time()
+
+                self.check_if_timeout_exceeded(timeout_started_at=timeout_started_at, step="click_join_audio_button", is_waiting_for_host_to_start_meeting=is_waiting_for_host_to_start_meeting)
 
                 last_check_timed_out = attempt_index == num_attempts_to_look_for_join_audio_button - 1
                 if last_check_timed_out:
