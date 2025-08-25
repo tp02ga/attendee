19193113a59979f1456fb35820ed3b1e18c8e818
Noah Duncan <crusty.noah@gmail.com>
2025-08-25 11:45:18 -0500

    rm ngrok

--- attendee/settings/development.py
+++ attendee/settings/development.py
@@ -3,7 +3,7 @@ import os
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["tendee-stripe-hooks.ngrok.io", "localhost", "6227447cea74.ngrok-free.app"]
+ALLOWED_HOSTS = ["tendee-stripe-hooks.ngrok.io", "localhost"]
 
 DATABASES = {
     "default": {
